8beed59492bf7867e5b4767ccf810228aadb85ed
Narayanayuvi <117725163+Narayanayuvi@users.noreply.github.com>
2023-03-15 17:43:36 +0530

    Inserting users details in the users table

---
+++ V1.2__Insertdata_users_table.sql
@@ -0,0 +1,2 @@
+INSERT INTO REGISTRATION_USERS(username,first_name,last_name,email,mobile) VALUES('peravali narayana','peravali','narayana','peravalinarayana@gmail.com','7731052033');
+INSERT INTO REGISTRATION_USERS(username,first_name,last_name,email,mobile) VALUES('Bala sai','Bala','Sai','balasai@gmail.com','8886666973');
